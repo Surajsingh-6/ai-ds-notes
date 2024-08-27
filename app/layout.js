df5e0ed7bfa80f3da20ee867e63eab9cc8bc0396
Suraj singh <suraj142005@gamil.com>
2024-08-27 07:15:23 +0530

    some changes

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import Head from "next/head"; // Import the Head component
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,6 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
+            <Head>
+                <meta name="google-adsense-account" content="ca-pub-7439049367478453" />
+                <title>{metadata.title}</title>
+                <meta name="description" content={metadata.description} />
+            </Head>
             <body className={inter.className}>
                 <Navbar />
                 {children}
